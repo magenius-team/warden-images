8e7da9b85e6a1d8a1f58349da1280da8162c2fc4
Denis Kopylov <dv.kopilov@gmail.com>
2023-07-09 17:18:30 +0300

    Remove php 7.3 from build

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 const PHP_LATEST = '8.2';
-const PHP_VERSIONS = ['7.3', '7.4', '8.0', '8.1', '8.2'];
+const PHP_VERSIONS = ['7.4', '8.0', '8.1', '8.2'];
 const NODE_LATEST = '19';
 const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19'];
 const EXPERIMENTAL_PHP_VERSIONS = [];
