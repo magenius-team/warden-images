d1438e22745b769f5ec7ab15c743f8777f0f5802
Denis Kopylov <dkopylov@magenius.team>
2024-08-01 00:49:30 +0300

    [PHP] Test build (no-task)

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -5,4 +5,4 @@ const PHP_VERSIONS = ['7.2', '7.4', '8.1', '8.2', '8.3'];
 const NODE_LATEST = '20';
 const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
 const EXPERIMENTAL_PHP_VERSIONS = [];
-const NOT_STABLE_XDEBUG_PHP_VERSIONS = [];
+const NOT_STABLE_XDEBUG_PHP_VERSIONS = ['7.2', '7.4'];
