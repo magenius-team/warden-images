b344df772a8f488b721b7b14452d071821c48ae0
Denis Kopylov <dkopylov@magenius.team>
2024-12-08 20:43:56 +0300

    Add php 8.4 build

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -1,7 +1,7 @@
 <?php
 
-const PHP_LATEST = '8.3';
-const PHP_VERSIONS = ['7.4', '8.1', '8.2', '8.3'];
+const PHP_LATEST = '8.4';
+const PHP_VERSIONS = ['7.4', '8.1', '8.2', '8.3', '8.4'];
 const NODE_LATEST = '20';
 const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
 const EXPERIMENTAL_PHP_VERSIONS = [];
