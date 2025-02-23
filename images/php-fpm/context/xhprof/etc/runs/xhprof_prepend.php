474bf0d3d005fcf6365e223143795a460f0b0020
Denis Kopylov <dkopylov@magenius.team>
2025-02-23 16:16:24 +0300

    [Xhprof] Send xhprof logs by register_shutdown_function

--- images/php-fpm/context/xhprof/etc/runs/xhprof_prepend.php
+++ images/php-fpm/context/xhprof/etc/runs/xhprof_prepend.php
@@ -6,4 +6,35 @@ if (extension_loaded('xhprof')) {
             'ignored_functions' => ['xhprof_disable']
         ]
     );
+
+    register_shutdown_function(function () {
+        $xhprofData = xhprof_disable();
+        $tags = [];
+        if (PHP_SAPI === 'cli') {
+            global $argv;
+            $tags['command'] = implode(' ', $argv);
+        } else {
+            $tags['url'] = $_SERVER['REQUEST_URI'];
+        }
+
+        $data = json_encode([
+            'profile' => $xhprofData,
+            'tags' => $tags,
+            'app_name' => getenv('WARDEN_ENV_NAME'),
+            'hostname' => gethostname(),
+            'date' => (new DateTimeImmutable())->getTimestamp()
+        ]);
+
+        try {
+            $ch = curl_init('http://buggregator:8000/api/profiler/store');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_exec($ch);
+        } catch (Exception $e) {
+            // Do nothing
+        }
+        curl_close($ch);
+    });
 }
